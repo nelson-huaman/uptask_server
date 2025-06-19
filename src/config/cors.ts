c88d64ca158b330e4b596d62dfffa450e7d8af4d
Nelson Huamán H. <libra.nelsonhuaman@gmail.com>
2025-06-18 22:47:22 -0500

    fix-cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,19 +1,15 @@
-import { CorsOptions } from "cors";
-import dotenv from "dotenv"
-dotenv.config()
+import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-   origin: function (origin, callback) {
-      const whitelist = [
-         process.env.FRONTEND_URL
-      ]
-      if(process.argv[2] === '--api') {
-         whitelist.push(undefined)
+   origin: (origin, callback) => {
+      const whitelist = [process.env.FRONTEND_URL];
+      if (process.argv[2] === '--api') {
+         whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
       }
-      if(whitelist.includes(origin)) {
-         callback(null, true);
+      if (!origin || whitelist.includes(origin)) {
+         callback(null, true); // Permitir acceso
       } else {
-         callback(new Error("Error de CORS"));
+         callback(new Error('Error de CORS'))
       }
    }
 }
